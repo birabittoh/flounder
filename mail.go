38303440073390bdb4d99f265f44cdbb41680192
alex wennerberg <alex@alexwennerberg.com>
2020-12-27 21:57:23 -0800

    Revert "cc me"
    This reverts commit 728cc51a177ae650efefdcd9491d37d072173fbf.

--- mail.go
+++ mail.go
@@ -9,7 +9,7 @@ func SendEmail(email string, subject string, body string) {
 	auth := smtp.PlainAuth("", c.SMTPUsername, c.SMTPPassword, "smtp.migadu.com")
 	msg := "From: " + c.SMTPUsername + "\n" +
 		"To: " + email + "\n" +
-		"Cc: " + "alex@alexwennerberg.com" + "\n" + // TODO remove hardcode
+		"Cc: " + c.SMTPUsername + "\n" +
 		"Subject:" + subject + "\n" +
 		body
 	err := smtp.SendMail(c.SMTPServer, auth, c.SMTPUsername, []string{email}, []byte(msg))
