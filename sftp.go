fe8eaeb180788db469c7fdd233ea825056209763
alex wennerberg <alex@alexwennerberg.com>
2021-02-26 19:27:15 -0800

    Simple password auth
    Still need to do a bit of a security check here

--- sftp.go
+++ sftp.go
@@ -4,7 +4,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -35,7 +34,6 @@ func (con *Connection) Fileread(request *sftp.Request) (io.ReaderAt, error) {
 
 func (con *Connection) Filewrite(request *sftp.Request) (io.WriterAt, error) {
 	// check user perms -- cant write others files
-	// check if file is inside your directory -- strings prefix?
 	fullpath := path.Join(c.FilesDirectory, filepath.Clean(request.Filepath))
 	userDir := getUserDirectory(con.User) // NOTE -- not cross platform
 	if strings.HasPrefix(fullpath, userDir) {
@@ -92,32 +90,23 @@ func buildHandlers(connection *Connection) sftp.Handlers {
 // Based on example server code from golang.org/x/crypto/ssh and server_standalone
 func runSFTPServer() {
 
-	var (
-		readOnly    bool
-		debugStderr bool
-	)
-
-	flag.BoolVar(&readOnly, "R", false, "read-only server")
-	flag.BoolVar(&debugStderr, "e", false, "debug to stderr")
-	flag.Parse()
-
-	debugStream := ioutil.Discard
-	if debugStderr {
-		debugStream = os.Stderr
-	}
-
 	// An SSH server is represented by a ServerConfig, which holds
 	// certificate details and handles authentication of ServerConns.
 	config := &ssh.ServerConfig{
-		// PublicKeyCallback
 		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
 			// Should use constant-time compare (or better, salt+hash) in
 			// a production setting.
-			fmt.Fprintf(debugStream, "Login: %s\n", c.User())
-			if c.User() == "alex" && string(pass) == "alex" {
+			if isOkUsername(c.User()) != nil { // extra check, probably unnecessary
+				return nil, fmt.Errorf("Invalid username")
+			}
+			_, _, err := checkLogin(c.User(), string(pass))
+			// TODO maybe give admin extra permissions?
+			fmt.Fprintf(os.Stderr, "Login: %s\n", c.User())
+			if err != nil {
+				return nil, fmt.Errorf("password rejected for %q", c.User())
+			} else {
 				return nil, nil
 			}
-			return nil, fmt.Errorf("password rejected for %q", c.User())
 		},
 	}
 
@@ -152,7 +141,7 @@ func runSFTPServer() {
 		log.Fatal("failed to handshake", err)
 	}
 	log.Println("login detected:", sconn.User())
-	fmt.Fprintf(debugStream, "SSH server established\n")
+	fmt.Fprintf(os.Stderr, "SSH server established\n")
 
 	// The incoming Request channel must be serviced.
 	go ssh.DiscardRequests(reqs)
@@ -162,33 +151,33 @@ func runSFTPServer() {
 		// Channels have a type, depending on the application level
 		// protocol intended. In the case of an SFTP session, this is "subsystem"
 		// with a payload string of "<length=4>sftp"
-		fmt.Fprintf(debugStream, "Incoming channel: %s\n", newChannel.ChannelType())
+		fmt.Fprintf(os.Stderr, "Incoming channel: %s\n", newChannel.ChannelType())
 		if newChannel.ChannelType() != "session" {
 			newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
-			fmt.Fprintf(debugStream, "Unknown channel type: %s\n", newChannel.ChannelType())
+			fmt.Fprintf(os.Stderr, "Unknown channel type: %s\n", newChannel.ChannelType())
 			continue
 		}
 		channel, requests, err := newChannel.Accept()
 		if err != nil {
 			log.Fatal("could not accept channel.", err)
 		}
-		fmt.Fprintf(debugStream, "Channel accepted\n")
+		fmt.Fprintf(os.Stderr, "Channel accepted\n")
 
 		// Sessions have out-of-band requests such as "shell",
 		// "pty-req" and "env".  Here we handle only the
 		// "subsystem" request.
 		go func(in <-chan *ssh.Request) {
 			for req := range in {
-				fmt.Fprintf(debugStream, "Request: %v\n", req.Type)
+				fmt.Fprintf(os.Stderr, "Request: %v\n", req.Type)
 				ok := false
 				switch req.Type {
 				case "subsystem":
-					fmt.Fprintf(debugStream, "Subsystem: %s\n", req.Payload[4:])
+					fmt.Fprintf(os.Stderr, "Subsystem: %s\n", req.Payload[4:])
 					if string(req.Payload[4:]) == "sftp" {
 						ok = true
 					}
 				}
-				fmt.Fprintf(debugStream, " - accepted: %v\n", ok)
+				fmt.Fprintf(os.Stderr, " - accepted: %v\n", ok)
 				req.Reply(ok, nil)
 			}
 		}(requests)
